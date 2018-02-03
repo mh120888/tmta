a77188f28c94c42255285c4ebe5921b6f27ba4ec
Matthew Higgins <matthew.alan.higgins@gmail.com>
2018-02-03 11:07:50 -0500

    Adds tests for guest/logged in user

--- test/tmta_web/controllers/page_controller_test.exs
+++ test/tmta_web/controllers/page_controller_test.exs
@@ -1,11 +1,38 @@
 defmodule TmtaWeb.PageControllerTest do
-  use TmtaWeb.ConnCase
+  use TmtaWeb.ConnCase, async: true
+  alias Tmta.Coherence.User
+  alias Tmta.Repo
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    response = html_response(conn, 200)
-    assert response =~ "TMTA"
-    assert response =~ "Helping you keep track of your vacations"
-    assert response =~ "<a href=\"#\" class=\"btn btn-primary\">Add a Trip</a>"
+  describe "Given any user" do
+    test "GET / displays TMTA and the welcome text", %{conn: conn} do
+      conn = get conn, "/"
+      response = html_response(conn, 200)
+      assert response =~ "TMTA"
+      assert response =~ "Helping you keep track of your vacations"
+    end
+  end
+
+  describe "Given a guest user" do
+    test "GET / displays Sign In and Sign Up links", %{conn: conn} do
+      conn = get conn, "/"
+      response = html_response(conn, 200)
+      assert response =~ "Sign In"
+      assert response =~ "Sign Up"
+    end
+  end
+
+  describe "Given a logged in user" do
+    setup [:create_user_and_log_in]
+    test "GET / displays the current user's name", %{conn: conn} do
+      conn = get conn, "/"
+      response = html_response(conn, 200)
+      assert response =~ "Hello Bob Smith"
+    end
+  end
+
+  defp create_user_and_log_in(m) do
+    user = User.changeset(%User{}, %{name: "Bob Smith", email: "test@example.com", password: "test", password_confirmation: "test"})
+    |> Repo.insert!
+    {:ok, conn: assign(m.conn, :current_user, user), user: user}
   end
 end
